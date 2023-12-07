98e7bf9f256258640a2c2211f00ecd3b2182a032
plusboy <no1plus88@gmail.com>
2023-12-07 10:04:49 +0800

    删除主体对象分组 同时删除里面的数据集和数据源

--- core/backend/src/main/java/io/dataease/service/datasource/DatasourceGroupService.java
+++ core/backend/src/main/java/io/dataease/service/datasource/DatasourceGroupService.java
@@ -1,18 +1,30 @@
 package io.dataease.service.datasource;
 
+import io.dataease.commons.constants.SysLogConstants;
 import io.dataease.commons.utils.AuthUtils;
 import io.dataease.commons.utils.BeanUtils;
+import io.dataease.commons.utils.DeLogUtils;
 import io.dataease.controller.ResultHolder;
 import io.dataease.controller.datasource.request.InsertDataSourceGroupRequest;
+import io.dataease.controller.request.dataset.DataSetTableRequest;
+import io.dataease.dto.SysLogDTO;
+import io.dataease.plugins.common.base.domain.DatasetTable;
+import io.dataease.plugins.common.base.domain.Datasource;
 import io.dataease.plugins.common.base.domain.DatasourceGroup;
 import io.dataease.plugins.common.base.mapper.DatasourceGroupMapper;
+import io.dataease.service.dataset.DataSetTableService;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class DatasourceGroupService {
+    @Resource
+    private DataSetTableService dataSetTableService;
+    @Resource
+    private DatasourceService datasourceService;
     @Resource
     private DatasourceGroupMapper datasourceGroupMapper;
 
@@ -39,9 +51,24 @@ public class DatasourceGroupService {
         return line > 0 ? ResultHolder.successMsg("更新分组成功") : ResultHolder.error("更新分组失败");
     }
 
-    public ResultHolder delete(String id) {
+    public ResultHolder delete(String id) throws Exception {
         int line = datasourceGroupMapper.deleteById(id, AuthUtils.getUser().getUserId().toString());
         //TODO 删除其他的数据(数据集 主题对象 等)
+        //查询分组里的数据集
+        DataSetTableRequest dataSetTableRequest = new DataSetTableRequest();
+        dataSetTableRequest.setGroupId(id);
+        List<DatasetTable> datasetTables = dataSetTableService.listByGroup(dataSetTableRequest);
+        //删除数据集和数据源
+        for (DatasetTable datasetTable : datasetTables) {
+            //数据集id
+            String datasetTableId = datasetTable.getId();
+            dataSetTableService.delete(id);
+            //数据源id
+            String dataSourceId = datasetTable.getDataSourceId();
+            Datasource datasource = datasourceService.get(dataSourceId);
+            SysLogDTO sysLogDTO = DeLogUtils.buildLog(SysLogConstants.OPERATE_TYPE.DELETE, SysLogConstants.SOURCE_TYPE.DATASOURCE, dataSourceId, datasource.getType(), null, null);
+            ResultHolder resultHolder = datasourceService.deleteDatasource(dataSourceId);
+        }
         return line > 0 ? ResultHolder.successMsg("删除分组成功") : ResultHolder.error("删除分组失败");
     }
 
