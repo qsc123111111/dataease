523b0998171a2ac8f68040f69bc3e983baee6671
plusboy <no1plus88@gmail.com>
2024-01-29 13:03:58 +0800

    修复excel数据集删除不正确

--- core/backend/src/main/java/io/dataease/service/dataset/DataSetTableService.java
+++ core/backend/src/main/java/io/dataease/service/dataset/DataSetTableService.java
@@ -442,6 +442,9 @@ public class DataSetTableService {
                     info.setExcelSheetDataList(excelSheetDataList);
                     sheetTable.setInfo(new Gson().toJson(info));
                     datasetTableMapper.insert(sheetTable);
+                    //将数据集和数据源关联信息写入 dataset_ref
+                    DatasetRef datasetRef = new DatasetRef(sheetTable.getId(), null);
+                    datasetRefMapper.insert(datasetRef);
                     sysAuthService.copyAuth(sheetTable.getId(), SysAuthConstants.AUTH_SOURCE_TYPE_DATASET);
                     saveExcelTableField(sheetTable.getId(), sheet.getFields(), true);
                     datasetIdList.add(sheetTable.getId());
