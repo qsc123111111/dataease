8128bafd5ebb39c373b628f6f95bb3c0df4b9c0f
plusboy <no1plus88@gmail.com>
2024-02-02 16:50:12 +0800

    修复添加用户失败

---
+++ core/backend/src/main/java/io/dataease/controller/themeModelPortal/ThemeModelPortalController.java
@@ -0,0 +1,11 @@
+package io.dataease.controller.themeModelPortal;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "主题模型门户")
+@RequestMapping("/themeModelPortal")
+@RestController
+public class ThemeModelPortalController {
+}
